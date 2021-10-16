56f5bced268f695fe24e5d7b8b4cee79babef113
D3_49685_Amol <amolmmore114@gmail.com>
2021-10-16 19:45:07 +0530

    Add method added in string calculator

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,34 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    	
+    	if(input.equals("")){
+			return 0;
+		}
+		else{
+			String delimiter = ",";
+			if(input.matches("//(.)\n(.)")){
+				delimiter = Character.toString(input.charAt(2));
+				input = input.substring(4);
+			}
+			
+			String numList[] = splitNumbers(input, delimiter + "|\n");
+			return sum(numList);
+			
+			
+			}
+    	
+		}
+    private int sum(String[] numList) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	private String[] splitNumbers(String input, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private static int toInt(String number){
+		return Integer.parseInt(number);
     }
 
 }
